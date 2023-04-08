90affe1f19014e4cde63bbd01de914b0ce6143d7
xoheveras <57674283+xoheveras@users.noreply.github.com>
2023-04-09 01:51:12 +0300

    Update States.py

--- states/States.py
+++ states/States.py
@@ -1,8 +1,1 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
-
-# class WorkRequests(StatesGroup):
-#     userLink = State()
-#     feedbackLink = State()
-#     onlineTime = State()
-#     descriptionWork = State()
-#     sendAdmin = State()
