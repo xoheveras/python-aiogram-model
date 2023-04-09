28a8710dc7d2c087ffadeeac5178e5b34ddb3451
xoheveras <57674283+xoheveras@users.noreply.github.com>
2023-04-10 02:16:11 +0300

    Update middleware.py

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,9 +1,13 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher.handler import CancelHandler 
 from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
 from aiogram.types import Message, CallbackQuery
 from loader import db
+import time
 import keyboards.keyboard as kb
+
+user_ban_status = {}
 
 class UserBanned(BaseMiddleware):
     async def on_process_message(self, message: Message, data: dict):
@@ -12,17 +16,16 @@ class UserBanned(BaseMiddleware):
             await message.answer("<b>Я не могу тебя пустить дальше, у тебя нет @username</b>")
             raise CancelHandler
 
-        try:
-            user = db.get("SELECT ban FROM users WHERE userid = ?", (message.from_user.id, ))[0]
-        except:
-            user = 0
+        user_id = message.from_user.id
+        if user_id in user_ban_status:
+            is_banned = user_ban_status[user_id]
+        else:
+            is_banned = db.get("SELECT ban FROM users WHERE userid = ?", (user_id, ))[0]
+            user_ban_status[user_id] = is_banned
 
-        if user == 1:
-            await message.answer(
-                '<b>🔥 Вы заблокированы</b>'
-            )
+        if is_banned == 1:
+            await message.answer('<b>🔥 Вы заблокированы</b>')
             raise CancelHandler
-
 
     async def on_process_callback_query(self, call: CallbackQuery, data: dict):
         pass
@@ -31,13 +34,43 @@ class UserBanned(BaseMiddleware):
             await call.answer("<b>Я не могу тебя пустить дальше, у тебя нет @username</b>", show_alert=True)
             raise CancelHandler
 
-        try:
-            user = db.get("SELECT ban FROM users WHERE userid = ?", (call.from_user.id, ))[0]
-        except:
-            user = 0
+        user_id = call.from_user.id
+        if user_id in user_ban_status:
+            is_banned = user_ban_status[user_id]
+        else:
+            is_banned = db.get("SELECT ban FROM users WHERE userid = ?", (user_id, ))[0]
+            user_ban_status[user_id] = is_banned
 
-        if user == 1:
-            await call.answer(
-                '<b>🔥 Вы заблокированы</b>'
-            )
+        if is_banned == 1:
+            await call.answer('<b>🔥 Вы заблокированы</b>')
+            raise CancelHandler
+            
+            
+class UserSpam(LifetimeControllerMiddleware):
+    MAX_MESSAGES_PER_MINUTE = 5
+    TIME_RANGE_IN_SECONDS = 60
+
+    def __init__(self):
+        super().__init__()
+
+        self.user_messages_count = {}
+
+    async def on_pre_process_message(self, message: Message, data: dict):
+        user_id = message.from_user.id
+        current_time = time.time()
+
+        if user_id not in self.user_messages_count:
+            self.user_messages_count[user_id] = []
+
+        # Удаление старых сообщений из списка
+        self.user_messages_count[user_id] = list(filter(
+            lambda timestamp: timestamp > current_time - self.TIME_RANGE_IN_SECONDS,
+            self.user_messages_count[user_id]))
+
+        # Добавление нового сообщения в список
+        self.user_messages_count[user_id].append(current_time)
+
+        # Если количество сообщений превышает лимит, отменяем обработку сообщения
+        if len(self.user_messages_count[user_id]) > self.MAX_MESSAGES_PER_MINUTE:
+            await message.answer("Слишком много сообщений. Подождите немного и попробуйте еще раз.")
             raise CancelHandler
